1d5c4f267f8416d366600b326b248df3614ac2f1
ggodkin <grgodkin@yahoo.com>
2025-02-20 20:46:29 -0700

    OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <ArduinoOTA.h>
 #include <driver/i2s.h>
 #include <WiFi.h>
 #include <WebServer.h>
@@ -212,11 +213,47 @@ void setup() {
   server.on("/equalizefreq", handleEqualizeFreq);
   server.begin();
   Serial.println("HTTP server started");
+
+  // OTA setup
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 }
 
 void loop() {
   // Handle web server requests
   server.handleClient();
+
+  // Handle OTA updates
+  ArduinoOTA.handle();
 
   // Stream the waveform to the DAC in smaller chunks
   size_t bytes_written;
